826876c4ac83e5553310b62be4390fd1ae2d8d4e
akmaljon <akmaljon@gmail.com>
2024-03-19 05:18:34 +0500

    My Works

---
+++ js/app.js
@@ -0,0 +1,23 @@
+ [{
+    title: "Pixer-project",
+    githubLink: "https://github.com/Tursunboev/Pixer-project.git",
+    vercelLink: "https://pixer-project-xzow.vercel.app/",
+  },
+];
+
+const template = document.querySelector("template");
+const worksList = document.querySelector(".works__list");
+
+projects.forEach((project) => {
+  const clone = template.content.cloneNode(true);
+
+  const title = clone.querySelector(".works__list__title");
+  const linkGithub = clone.querySelector(".works__list__link__github");
+  const linkVercel = clone.querySelector(".works__list__link__vercel");
+
+  title.textContent = project.title;
+  linkGithub.href = project.githubLink;
+  linkVercel.href = project.vercelLink;
+
+  worksList.appendChild(clone);
+});
